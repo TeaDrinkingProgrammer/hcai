5b5a980012499c8e442a6e85c532312f1f20e0fc
TeaDrinkingProgrammer <teadrinkingprogrammer@proton.me>
2023-11-01 16:43:03 +0000

    Fixed bug. Improved layout

--- src/hcai/app/good_app/result.py
+++ src/hcai/app/good_app/result.py
@@ -9,26 +9,28 @@ from hcai.app.forms import FeedbackForm
 from hcai.app.models import Feedback
 
 def index(request: HttpRequest):
-    if request.method == "POST":
-        # Parse json from request body
-        data = json.loads(request.body)
-        # Get the rating from the request
-        rating = data.get('rating', None)
-        return HttpResponse(status=200)
     def calc_avg():
         feedbacks = Feedback.objects.all()
         avg = 0
         if len(feedbacks) > 0:
             avg = sum([feedback.rating for feedback in feedbacks]) / len(feedbacks)
-        return avg
+        return round(avg, 2), len(feedbacks)
+    
+    if request.method == "POST":
+        # Parse json from request body
+        data = json.loads(request.body)
+        # Get the rating from the request
+        rating = data.get('rating', None)
+        feedback = Feedback(rating=rating)
+        feedback.save()
+        return HttpResponse(status=200)
     
     rating = request.GET.get('rating', None)
     if rating is not None:
         # Get the rating from the request
-        feedback = Feedback(rating=rating)
-        feedback.save()
+        avg, n_votes = calc_avg()
         # After the rating is given, redirect to the result page
-        return render(request, "hcai/good_app/result.html", {"rating": rating})
+        return render(request, "hcai/good_app/result.html", {"avg": avg, "n_votes": n_votes, "rating": rating})
     else:
-        return render(request, "hcai/good_app/result.html",{"avg": calc_avg(), "rating": 0 })
+        return render(request, "hcai/good_app/result.html",{"avg": avg, "n_votes": n_votes, "rating": 0 })
     
