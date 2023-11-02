8c97dbfa98ae595c84bd1b1bd07be646f931d5b6
TeaDrinkingProgrammer <teadrinkingprogrammer@proton.me>
2023-11-02 14:59:29 +0000

    added gunicorn

--- src/hcai/app/good_app/home.py
+++ src/hcai/app/good_app/home.py
@@ -1,5 +1,14 @@
 from hcai.__init__ import *
 from django.shortcuts import render
 
+from hcai.app.models import GoodAppFeedback
+
 def index(request):
-    return render(request, "hcai/good_app/home.html")
+    def calc_avg():
+        feedbacks = GoodAppFeedback.objects.all()
+        avg = 0
+        if len(feedbacks) > 0:
+            avg = sum([feedback.rating for feedback in feedbacks]) / len(feedbacks)
+        return round(avg, 2), len(feedbacks)
+    avg_feedback, n_feedback = calc_avg()
+    return render(request, "hcai/good_app/home.html", {"avg_feedback": avg_feedback, "n_feedback": n_feedback})
